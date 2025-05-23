aef612d57bcfb8faacb9b07586060e26c2ed5384
Marenga <marengamatdrb@gmail.com>
2025-05-23 18:31:48 +0200

    Receive players

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::OnceLock;
+use std::{sync::OnceLock, u32};
 
 use axum::{
     Json, Router,
@@ -8,6 +8,7 @@ use axum::{
 };
 use chrono::Utc;
 use serde::Deserialize;
+use sf_api::gamestate::unlockables::EquipmentIdent;
 use sqlx::{
     Pool, Sqlite,
     sqlite::{
@@ -19,27 +20,26 @@ use sqlx::{
 static DB: OnceLock<Pool<Sqlite>> = OnceLock::new();
 
 #[allow(clippy::expect_used, clippy::missing_panics_doc)]
-pub fn get_state() -> Pool<Sqlite> {
+pub fn get_db() -> Pool<Sqlite> {
     DB.get()
         .expect("STATE always has to be initialize first")
         .clone()
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn core::error::Error>> {
     tracing_subscriber::fmt::init();
 
     let con_option = SqliteConnectOptions::new()
         .filename("mfbot.db")
         .journal_mode(SqliteJournalMode::Wal)
         .synchronous(SqliteSynchronous::Normal)
-        .optimize_on_close(true, Some(400))
+        .auto_vacuum(sqlx::sqlite::SqliteAutoVacuum::Incremental)
+        .foreign_keys(true)
+        .optimize_on_close(true, Some(u32::MAX))
         .create_if_missing(true);
 
-    let db = SqlitePoolOptions::new()
-        .connect_with(con_option)
-        .await
-        .unwrap();
+    let db = SqlitePoolOptions::new().connect_with(con_option).await?;
 
     sqlx::query(
         "CREATE TABLE IF NOT EXISTS error (
@@ -55,25 +55,134 @@ async fn main() {
     )",
     )
     .execute(&db)
-    .await
-    .unwrap();
+    .await?;
+
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS raw_player (
+            raw_player_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fetch_date TEXT NOT NULL,
+            name TEXT NOT NULL,
+            server TEXT NOT NULL,
+            info TEXT NOT NULL,
+
+            description TEXT,
+            guild TEXT,
+            soldier_advice INTEGER
+    )",
+    )
+    .execute(&db)
+    .await?;
+
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS server (
+            server_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            url TEXT NOT NULL
+        )",
+    )
+    .execute(&db)
+    .await?;
+
+    sqlx::query("CREATE INDEX IF NOT EXISTS server_url_idx on server (url)")
+        .execute(&db)
+        .await?;
+
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS player (
+            player_id INT NOT NULL,
+            server_id INT NOT NULL REFERENCES server(server_id),
+
+            name TEXT NOT NULL,
+            last_online INT NOT NULL,
+            last_updated INT NOT NULL,
+            PRIMARY KEY (server_id, player_id)
+        )",
+    )
+    .execute(&db)
+    .await?;
+
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS equipment (
+            server_id INT NOT NULL,
+            player_id INT NOT NULL,
+            slot INT NOT NULL,
+
+            ident INT,
+            PRIMARY KEY (server_id, player_id, slot),
+            FOREIGN KEY 
+                (server_id, player_id) REFERENCES player(server_id, player_id)
+        )",
+    )
+    .execute(&db)
+    .await?;
+
+    sqlx::query(
+        "CREATE INDEX IF NOT EXISTS equipment_lookup_idx on equipment (
+            server_id, ident, player_id
+        )",
+    )
+    .execute(&db)
+    .await?;
 
     DB.get_or_init(move || db);
 
     let app = Router::new()
         .route("/", get(root))
+        .route("/updatePlayers", post(report_players))
         .route("/report", post(report_bug));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:4949")
-        .await
-        .unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:4949").await?;
+    Ok(axum::serve(listener, app).await?)
 }
 
 async fn root() -> axum::response::Redirect {
     axum::response::Redirect::permanent("https://forum.mfbot.de/")
 }
 
+/// Compresses the original Equipment Ident into a single i32
+pub fn compress_ident(ident: EquipmentIdent) -> i32 {
+    let mut res = ident.model_id as i64; // 0..16
+    res |= (ident.color as i64) << 16; // 16..24
+    res |= (ident.typ as i64) << 24; // 24..28
+    res |= (ident.class.map(|a| a as i64).unwrap_or(0)) << 28; // 28..32
+    res as i32
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RawOtherPlayer {
+    name: String,
+    server: String,
+    info: String,
+    description: Option<String>,
+    guild: Option<String>,
+    soldier_advice: Option<i64>,
+    fetch_date: String,
+}
+
+async fn report_players(
+    Json(players): Json<Vec<RawOtherPlayer>>,
+) -> Result<(), Response> {
+    for player in players {
+        log::info!("Players reported: {player:?}");
+
+        sqlx::query(
+            "INSERT INTO raw_player 
+            (fetch_date, name, server, info, description, guild, \
+             soldier_advice) 
+            VALUES (?, ?, ?, ?, ?, ?, ?)",
+        )
+        .bind(player.fetch_date)
+        .bind(player.name)
+        .bind(player.server)
+        .bind(player.info)
+        .bind(player.description)
+        .bind(player.guild)
+        .bind(player.soldier_advice)
+        .execute(&get_db())
+        .await
+        .map_err(MFBotError::DBError)?;
+    }
+    Ok(())
+}
 
 #[derive(Debug, Deserialize)]
 pub struct BugReportArgs {
@@ -102,7 +211,7 @@ async fn report_bug(Json(args): Json<BugReportArgs>) -> Result<(), Response> {
     .bind(args.error_text)
     .bind(args.hwid)
     .bind(Utc::now().to_rfc3339())
-    .execute(&get_state())
+    .execute(&get_db())
     .await
     .map_err(MFBotError::DBError)?;
 

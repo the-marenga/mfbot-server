baa534951586fd53c2f1c57b5ee4ab3515e0a268
Marenga <marengamatdrb@gmail.com>
2025-05-31 01:14:56 +0200

    get characters force index

--- src/main.rs
+++ src/main.rs
@@ -441,6 +441,9 @@ pub async fn get_characters_to_crawl(
           WHERE server_id = $1
             AND next_report_attempt < $2
             AND is_removed = false
+          -- This forced an index to be used in my testing. Could also make
+          -- things worse, will have to test
+          ORDER BY next_report_attempt
           LIMIT $3 )
         UPDATE player
         SET next_report_attempt = $4
